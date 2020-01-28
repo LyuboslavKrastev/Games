1dbdd9c1e491be8a7b2105b1d10201584faded6e
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-01-28 13:26:48 +0200

    Asteroid
    - Added an Asteroid
    - Adding logic to start the game when
      the asteroid is shot.

---
+++ Space Warz/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private float _speed = 25;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, _speed) * Time.deltaTime);
+    }
+}
