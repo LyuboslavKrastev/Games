f9ab3d2b5887ac98b7c44b289b25f7a2c394503d
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-02-09 10:56:04 +0200

    Building a 2D Terminal hacking simulator

---
+++ Terminal Hacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    private const string greeting = "Welcome to the machine!";
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    private void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What would you like to hack?");
+        Terminal.WriteLine("Press 1 for Library");
+        Terminal.WriteLine("Press 2 for Polce Station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    void Update()
+    {
+        
+    }
+}
