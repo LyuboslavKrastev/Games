4de621779fe5c3d34e17c6f27efe712d1a127cd1
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-02-13 17:53:07 +0200

    Rocket Movement
    - Implementing rocket movement:
      boosters and rotation

---
+++ Arcade Rocket Flyer/Assets/Scripts/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        if (rigidbody == null)
+        {
+            Debug.LogError("RigidBody is NULL!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+            Vector3 force = Vector3.up;
+            rigidbody.AddRelativeForce(force * Time.deltaTime);
+            Debug.Log("thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A)) // cant rotate in both directions
+        {
+            // rotate left
+            transform.Rotate(Vector3.forward); // Z axis
+            Debug.Log("rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //rotate right
+            transform.Rotate(-Vector3.forward);
+            Debug.Log("rotating right");
+        }
+    }
+}
