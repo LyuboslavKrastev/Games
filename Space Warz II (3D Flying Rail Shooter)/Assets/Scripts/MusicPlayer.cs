a8eb363d11a121a77889a9aafd0d2739a66fee88
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-02-17 21:04:44 +0200

    Splash scene, audio and moving game scene camera
    - Added a splash scene which transitions into the game scene
    - Music is playing on the splash scene and it continues to play on the game scene
    - When the game scene is loaded, the main camera moves in a circuit within the terrain

---
+++ Space Warz II (3D Flying Rail Shooter)/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    void Start()
+    {
+        Invoke(nameof(LoadFirstScene), 2f);
+    }
+
+    // Update is called once per frame
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
