373e54145ffd223b1ff1f123da80b91e509be34f
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-02-17 17:59:10 +0200

    Additional functionallity for the custom cube
    - The X and Z coordinates of the
      custom cubes will now be displayed
      on their top.

---
+++ Tower Defense/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    // our current cube dimensions are 10, 10, 10, but we may decide to change them in the future
+    [Range(1f, 20f)] [SerializeField] private float _gridSize = 10f;
+
+    private TextMesh _textMesh;
+
+    void Awake()
+    {
+        _textMesh = GetComponentInChildren<TextMesh>();
+
+        if (_textMesh == null)
+        {
+            Debug.LogError("TextMesh is NULL!");
+        }
+    }
+
+    void Update()
+    {
+        // constrain the position of the block whenever we try to move something in the scene so we can more precisely build a path of cubes
+        float xPosition = Mathf.RoundToInt(transform.position.x / _gridSize) * _gridSize;
+        float zPosition = Mathf.RoundToInt(transform.position.z / _gridSize) * _gridSize;
+
+        _textMesh.text = $"{xPosition / _gridSize}, {zPosition / _gridSize}";
+
+        transform.position = new Vector3(xPosition, 0f, zPosition);
+    }
+}
