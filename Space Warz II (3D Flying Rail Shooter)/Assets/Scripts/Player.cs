d6cfcb7c45efae60ac0b38a00929aa2a9f70394e
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2020-02-22 19:49:38 +0200

    Player rotation, laser particles, obstacles
    - Configured player rotation
    - Added 3 laser-like particles to the player ship
    - Implementing obstacles on the circuit

--- Space Warz II (3D Flying Rail Shooter)/Assets/Scripts/Player.cs
+++ Space Warz II (3D Flying Rail Shooter)/Assets/Scripts/Player.cs
@@ -1,18 +1,44 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
     [Tooltip("in m/s")][SerializeField] float _speed = 10f;
 
-    [SerializeField] private float _xPositionBoundary = 5f;
-    [SerializeField] private float _yPositionBoundary = 3f;
+    [SerializeField] private float _xPositionBoundary = 8f;
+    [SerializeField] private float _yPositionBoundary = 5f;
+
+    float _horizontalInput = 0f;
+    float _verticalInput = 0f;
+
+    private float _positionPitchFactor = -5f;
+    private float _controlPitchFactor = -20f;
+
+    private float _positionYawFactor = 5f;
+
+    private float _controllRollFactor = -20f;
+
+
     void Update()
     {
-        CalculateMovement();
+        ProcessTranslation();
+        ProcessRotation();
     }
 
-    private void CalculateMovement()
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * _positionPitchFactor;
+        float pitchDueToControl =  + _verticalInput * _controlPitchFactor;
+        float pitch = pitchDueToControl + pitchDueToPosition;
+
+        float yaw = transform.localPosition.x *  _positionYawFactor;
+
+        float roll =  _horizontalInput * _controllRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
     {
         float constrainedXPosition = CalculateHorizontalMovement();
 
@@ -25,8 +51,8 @@ public class Player : MonoBehaviour
 
     private float CalculateHorizontalMovement()
     {
-        float horizontalInput = CrossPlatformInputManager.GetAxis("Horizontal");
-        float horizontalOffsetForFrame = horizontalInput * _speed * Time.deltaTime;
+        _horizontalInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        float horizontalOffsetForFrame = _horizontalInput * _speed * Time.deltaTime;
 
         float rawXPosition = transform.localPosition.x + horizontalOffsetForFrame;
         float constrainedXPosition = Mathf.Clamp(rawXPosition, -_xPositionBoundary, _xPositionBoundary); // restricting the position so that the player cannot fly off the screen
@@ -35,11 +61,11 @@ public class Player : MonoBehaviour
 
     private float CalculateVerticalMovement()
     {
-        float verticalInput = CrossPlatformInputManager.GetAxis("Vertical");
-        float verticalOffsetForFrame = verticalInput * _speed * Time.deltaTime;
+        _verticalInput = CrossPlatformInputManager.GetAxis("Vertical");
+        float verticalOffsetForFrame = _verticalInput * _speed * Time.deltaTime;
 
-        float rawYosition = transform.localPosition.y + verticalOffsetForFrame;
-        float constrainedYPosition = Mathf.Clamp(rawYosition, -_yPositionBoundary, _yPositionBoundary); // restricting the position so that the player cannot fly off the screen
+        float rawYPosition = transform.localPosition.y + verticalOffsetForFrame;
+        float constrainedYPosition = Mathf.Clamp(rawYPosition, -_yPositionBoundary, _yPositionBoundary); // restricting the position so that the player cannot fly off the screen
         return constrainedYPosition;
     }
 }
